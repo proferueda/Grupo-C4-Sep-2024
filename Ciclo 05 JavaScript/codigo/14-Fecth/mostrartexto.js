527f02147d42c614080f308cb1fc97c1dd9a77f3
chrueda <carlos.rueda@liceopatria.edu.co>
2024-12-12 22:08:14 -0500

    Remove outdated promise and callback presentations; add session storage example and login functionality

--- Ciclo 05 JavaScript/codigo/14-Fecth/mostrartexto.js
+++ Ciclo 05 JavaScript/codigo/14-Fecth/mostrartexto.js
@@ -1,7 +1,12 @@
 export default function mostrarTexto(elemHTML) {
     console.log("Funcion mostrarTexto");
     fetch("datos.csv")
-        .then(response => response.text())
+        .then(response => {
+            console.log("[RESPONSE]\n", response)
+            let text = response.text();
+            console.log("[RESPONSE.text()]\n", text);
+            return text;
+        })
         .then(data => {
             console.table(data);
             elemHTML.innerHTML = data;
